501d53fcbb1c84cbbc24169d1d7b0bc1303a594c
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-04-27 23:25:16 -0300

    Handle prisma email constraint violation

--- src/middlewares/exceptionHandler.ts
+++ src/middlewares/exceptionHandler.ts
@@ -1,5 +1,7 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { ValidationError } from "class-validator";
 import { NextFunction, Request, Response } from "express";
+import { PrismaError } from "prisma-error-enum";
 
 export function exceptionHandler(
   error: any,
@@ -7,6 +9,17 @@ export function exceptionHandler(
   response: Response,
   next: NextFunction
 ): Response {
+  if (error instanceof PrismaClientKnownRequestError) {
+    switch (error.code) {
+      case PrismaError.UniqueConstraintViolation:
+        if (isEmailConstraintViolation(error.meta)) {
+          return response.status(400).json({ error: "Email already in use" });
+        }
+      default:
+        return response.status(400).json(error);
+    }
+  }
+
   if (error[0] instanceof ValidationError) {
     const errorMessageArray = generateValidationErrorMessageArray(error);
 
@@ -28,4 +41,8 @@ function generateValidationErrorMessageArray(error: ValidationError[]) {
 
 function getValidationErrorDescription(error: ValidationError) {
   return Object.values(error.constraints).toString();
+}
+
+function isEmailConstraintViolation(errorMeta: object): boolean {
+  return Object.values(errorMeta)[0][0] === "email";
 }
