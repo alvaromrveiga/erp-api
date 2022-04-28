64c656bbb7eaa8e21b006d00058d4eebe3e0ae5f
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-04-28 16:05:56 -0300

    Add createAdminUser script
    Script for API first use, after that it is recommended to delete the admin user

---
+++ src/utils/createAdminUser.ts
@@ -0,0 +1,42 @@
+import { Department } from "@prisma/client";
+import { prisma } from "../../prisma/prismaClient";
+import { CreateUserDto } from "../models/user/dtos/CreateUserDto";
+import { CreateUserService } from "../models/user/useCases/createUser/CreateUserService";
+
+async function createAdminUser() {
+  try {
+    const departmentHR = await getHRDepartment();
+
+    const createUserService = new CreateUserService();
+
+    await prisma.user.delete({ where: { email: "admin@example.com" } });
+
+    const data = {
+      email: "admin@example.com",
+      name: "Admin",
+      password: process.env.ADMIN_PASSWORD,
+      role: "ADMIN",
+      departmentId: departmentHR.id,
+    } as CreateUserDto;
+
+    await createUserService.execute(data);
+  } catch (error) {}
+}
+
+async function getHRDepartment(): Promise<Department> {
+  let departmentHR = await prisma.department.findUnique({
+    where: { department: "Human Resources" },
+  });
+
+  if (!departmentHR) {
+    departmentHR = await prisma.department.create({
+      data: {
+        department: "Human Resources",
+      },
+    });
+  }
+
+  return departmentHR;
+}
+
+createAdminUser();
