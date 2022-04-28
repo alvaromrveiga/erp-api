f34b8cb9ae7fbfa68927a6a5d7d04fee94ac520a
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-04-28 18:43:06 -0300

    Include user email and departmant name

--- src/models/task/useCases/findAllTasks/FindAllTasksService.ts
+++ src/models/task/useCases/findAllTasks/FindAllTasksService.ts
@@ -5,7 +5,12 @@ export class FindAllTasksService {
   constructor(private tasksRepository = prisma.tasks) {}
 
   async execute(): Promise<Task[]> {
-    const tasks = await this.tasksRepository.findMany();
+    const tasks = await this.tasksRepository.findMany({
+      include: {
+        department: { select: { department: true } },
+        user: { select: { email: true } },
+      },
+    });
 
     return tasks;
   }
