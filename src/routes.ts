c91ae899b3cbe978d5fde474255e930b9c6f36bd
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-04-28 16:06:43 -0300

    Rename file to ensureRoles

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { Roles } from "@prisma/client";
 import { Router } from "express";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
-import { ensureRoles } from "./middlewares/ensureRole";
+import { ensureRoles } from "./middlewares/ensureRoles";
 import { LoginController } from "./models/auth/useCases/login/LoginController";
 import { CreateUserController } from "./models/user/useCases/createUser/CreateUserController";
 import { FindUserByEmailController } from "./models/user/useCases/findUserByEmail/FindUserByEmailController";
